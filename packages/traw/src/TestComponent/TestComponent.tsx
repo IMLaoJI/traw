1a62ff9c1d8493fe84bc8e827563b530d9dc6a1a
kyumin <kyumin@traw.io>
2022-12-01 14:14:39 +0900

    Add tailwind css

--- packages/traw/src/TestComponent/TestComponent.tsx
+++ packages/traw/src/TestComponent/TestComponent.tsx
@@ -5,8 +5,8 @@ import { TestComponentProps } from "./TestComponent.types";
 import "./TestComponent.css";
 
 const TestComponent: React.FC<TestComponentProps> = ({ heading, content }) => (
-  <div data-testid="test-component" className="test-component">
-    <h1 data-testid="test-component__heading" className="heading">
+  <div data-testid="test-component" className="test-component bg-slate-100 font-bold">
+    <h1 data-testid="test-component__heading" className="heading font-bold">
       {heading + "dasdff"}
     </h1>
     <div data-testid="test-component__content">{content}</div>
