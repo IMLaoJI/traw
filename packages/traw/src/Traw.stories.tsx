067e90ac6eb3267225f7dd02434b835a5f1544a4
kyumin <kyumin@traw.io>
2022-12-01 15:35:53 +0900

    Fix traw story

--- packages/traw/src/Traw.stories.tsx
+++ packages/traw/src/Traw.stories.tsx
@@ -1,7 +1,7 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
 
-import Traw from './Traw';
+import { Traw } from './Traw';
 
 export default {
   title: 'Traw/Traw',
@@ -11,7 +11,7 @@ export default {
 } as ComponentMeta<typeof Traw>;
 
 const Template: ComponentStory<typeof Traw> = () => (
-  <div className="h-screen flex">
+  <div className="h-screen flex -m-4">
     <Traw />
   </div>
 );
