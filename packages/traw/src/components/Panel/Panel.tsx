f886c92161ec283db9d5b422aecb9d5a0f5a0fae
kyumin <kyumin@traw.io>
2022-12-06 17:35:49 +0900

    Add mocking props to blockItem

--- packages/traw/src/components/Panel/Panel.tsx
+++ packages/traw/src/components/Panel/Panel.tsx
@@ -19,6 +19,7 @@ const Panel = ({ handlePlayClick }: PanelProps) => {
                 userName="userName"
                 date="오전 10:00"
                 blockText="동구밖 과수원길 아카시아 꽃이 활짝폈네 "
+                isVoiceBlock={false}
                 handlePlayClick={handlePlayClick}
               />
             </ul>
