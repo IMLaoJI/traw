184b2e964ea08f04a9941e7193cacdf4b2205c0b
kyungjin <kyungjin@traw.io>
2022-12-06 15:55:59 +0900

    panel lasyout

--- packages/traw/src/components/Panel/Panel.tsx
+++ packages/traw/src/components/Panel/Panel.tsx
@@ -1,9 +1,65 @@
-import React from 'react';
+import React from "react";
 
-const Panel = () => {
+interface PanelProps {
+  handlePlayClick: () => void;
+}
+
+const Panel = ({ handlePlayClick }: PanelProps) => {
   return (
-    <div className="flex flex-1 flex-row shadow-3xl items-center justify-center">
-      I&apos;m panel
+    <div className="relative">
+      <div className="flex flex-col h-full shadow-3xl items-center rounded-2xl  bg-white  w-[269px] p-2 ">
+        <div className="absolute left-0 right-0 top-0 bottom-0 p-2 flex flex-col ">
+          {/* Header */}
+          <header className="flex pt-2 w-full gap-3">
+            <button
+              className="text-traw-purple text-xs pl-3"
+              onClick={handlePlayClick}
+            >
+              재생
+            </button>
+            <div className="text-traw-grey-dark text-xs">
+              00:00:00 / 00:00:00
+            </div>
+          </header>
+          {/* Body */}
+          <div className="flex flex-col  mt-3 flex-[2_2_auto] w-full overflow-y-auto min-h-0 pl-3">
+            <ul className="">
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+              <li>Hello World</li>
+            </ul>
+          </div>
+          {/* Footer */}
+          <footer className="">Footer</footer>
+        </div>
+      </div>
     </div>
   );
 };
