b42404dad82cfcd51143eb1900ff8a087b10123a
kyungjin <kyungjin@traw.io>
2022-12-08 15:04:47 +0900

    Style Buttons hover

--- packages/traw/src/components/Panel/Panel.tsx
+++ packages/traw/src/components/Panel/Panel.tsx
@@ -12,7 +12,7 @@ const Panel = ({ handlePlayClick }: PanelProps) => {
     <div className="relative w-full ">
       <div className="flex flex-col w-full sm:w-[269px] h-full items-center bg-white rounded-2xl shadow-[0_10px_60px_rgba(189,188,249,0.5)]">
         <div className="absolute left-0 right-0 top-0 bottom-0 p-2 flex flex-col ">
-          <PanelHeader handlePlayClick={handlePlayClick} />
+          <PanelHeader handlePlayClick={handlePlayClick} isPlaying={true} />
           <div className="flex flex-col  mt-3 flex-[2_2_auto] w-full overflow-y-auto min-h-0 px-2">
             <ul className="">
               <BlockItem
---
+++ packages/traw/src/icons/pause.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+const SvgPause = (props) => (
+  <svg
+    id="pause_svg__Outline"
+    xmlns="http://www.w3.org/2000/svg"
+    x={0}
+    y={0}
+    viewBox="0 0 512 512"
+    style={{
+      enableBackground: "new 0 0 512 512",
+    }}
+    xmlSpace="preserve"
+    {...props}
+  >
+    <style>{".pause_svg__st0{fill:#fff}.pause_svg__st1{fill:#f189b3}"}</style>
+    <path d="M193.9 396.5c-19.2 0-34.7-15.6-34.7-34.7V150.2c0-19.2 15.6-34.7 34.7-34.7 19.2 0 34.7 15.6 34.7 34.7v211.5c.1 19.3-15.5 34.8-34.7 34.8zM321.3 396.5c-19.2 0-34.7-15.6-34.7-34.7V150.2c0-19.2 15.6-34.7 34.7-34.7 19.2 0 34.7 15.6 34.7 34.7v211.5c0 19.3-15.5 34.8-34.7 34.8z" />
+  </svg>
+);
+export default SvgPause;
