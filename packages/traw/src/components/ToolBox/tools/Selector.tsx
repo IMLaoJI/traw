1e5949ad4825d266e1c2c5a8b44885e7ee6ebeb3
kyungjin <kyungjin@traw.io>
2022-12-26 10:26:02 +0900

    Remove legacy toolbox

--- packages/traw/src/components/ToolBox/tools/Selector.tsx
+++
@@ -1,9 +0,0 @@
-import SvgSelector from '../../../icons/selector';
-import { TrawToolInfo } from '../../../types';
-
-export const info: TrawToolInfo = {
-  type: 'select',
-  Icon: SvgSelector,
-  label: 'Selector (V or 1)',
-  shortcut: ['V', 1],
-};
